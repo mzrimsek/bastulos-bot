a23922d5fd5e830eead7df7920dd89bbe72ddafd
Mike Zrimsek <mikezrimsek@gmail.com>
2021-06-15 21:58:38 -0400

    we only are going to use the one channel - no need for several

--- src/config/twitch.ts
+++ src/config/twitch.ts
@@ -1,12 +1,7 @@
 import { getEnvValue } from '../utils';
 
-const channelKeys = Object.keys(process.env).filter(key =>
-  key.match(/TWITCH_CHANNEL_\d/g)
-);
-const channels = channelKeys.map(key => getEnvValue(key));
-
 export default {
-  channels,
+  channel: getEnvValue('TWITCH_CHANNEL'),
   clientId: getEnvValue('TWITCH_APPLICATION_CLIENT_ID'),
   clientSecret: getEnvValue('TWITCH_APPLICATION_CLIENT_SECRET'),
   tokensLocation: getEnvValue('TWITCH_BOT_TOKENS_FILE_LOCATION')
